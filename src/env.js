53ccebc4175ba6c081695a3e60c81d5b08912f3a
example <denilsson.d.sousa@gmail.com>
2022-08-03 11:55:59 -0400

    jkhjk

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
 export default {
-  apiUrl: 'https://jagao.dev/backend/app/public/api/'
-  // apiUrl: 'http://localhost:8000/api/'
+  // apiUrl: 'https://jagao.dev/backend/app/public/api/'
+  apiUrl: 'http://localhost:8000/api/'
 }
